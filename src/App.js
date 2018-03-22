220b47de1b29f69ff965ed745995468ddb5dccb2
Liviu Ioan Iancu <liviu.iancu@tim-mb-150.3pillar.corp>
2018-03-22 13:47:30 +0200

    Store history for later use

--- src/App.js
+++ src/App.js
@@ -6,28 +6,81 @@ import GameGrid from './GameGrid';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.handleStart = this.handleStart.bind(this);    
     this.state = {
       onStart: 'hidden',
+      xIsNext: true,
       history: [
-        {id:0, val:''},
-        {id:1, val:''},
-        {id:2, val:''},
-        {id:3, val:''},
-        {id:4, val:''},
-        {id:5, val:''},
-        {id:6, val:''},
-        {id:7, val:''},
-        {id:8, val:''}
+        {squares: [
+          {id:0, val:''},
+          {id:1, val:''},
+          {id:2, val:''},
+          {id:3, val:''},
+          {id:4, val:''},
+          {id:5, val:''},
+          {id:6, val:''},
+          {id:7, val:''},
+          {id:8, val:''}
+        ]}
       ]
     }
+    this.handleStart = this.handleStart.bind(this);
+    this.calculateWinner = this.calculateWinner.bind(this);
   }
   handleStart() {
     this.setState({
       onStart: ''
     });
   }
+  handleClickSquare(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if(this.calculateWinner(squares) || squares[i].val) {
+      return;
+    }
+    squares[i].val = this.state.xIsNext ? 'x' : 'o';
+    this.setState({
+      gameSquares: squares,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a].val && squares[a].val === squares[b].val && squares[a].val === squares[c].val) {
+        console.error('1');
+        return 'Winner is: ' + squares[a].val;
+      }
+      if (!squares.some((item)=>item.val === '')) {
+        console.error('2');
+        return 'Game ended in draw';
+      }
+    }
+    return null;
+  }
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const gameState = this.calculateWinner(current.squares);
+    let status;
+
+    if(gameState) {
+      status = gameState;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="app">
         <header className="app-header">
@@ -43,9 +96,25 @@ class App extends Component {
         </header>
         
         <div className="game-container">
-          <GameGrid 
-            board={this.state.history}
-            hide={this.state.onStart}/>
+
+          <div className={this.state.onStart}>
+            <nav className="navbar navbar-inverse">
+              <div className="container-fluid">
+                <div className="navbar-header">
+                  <a className="navbar-brand">
+                    {status}
+                  </a>
+                </div> 
+              </div>
+            </nav>
+
+            <GameGrid 
+              gameSquares={current.squares}
+              onClick={(i) => this.handleClickSquare(i)}
+              show={this.state.onStart}
+              className={this.state.onStart} />
+          </div>
+
         </div>
       </div>
     );
--- src/GameGrid.js
+++ src/GameGrid.js
@@ -1,81 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import GridItem from './GridItem';
 
-class GameGrid extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      gameSquares: this.props.board,
-      xIsNext: true
-    };
-  }
-  updateSquare(i) {
-    const squares = this.state.gameSquares.slice();
-    if(this.calculateWinner(squares) || squares[i].val) {
-      return;
-    }
-    squares[i].val = this.state.xIsNext ? 'x' : 'o';
-    this.setState({
-      gameSquares: squares,
-      xIsNext: !this.state.xIsNext
-    });
-  }
-  calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a].val && squares[a].val === squares[b].val && squares[a].val === squares[c].val) {
-        return 'Winner is: ' + squares[a].val;
-      }
-      if (!this.state.gameSquares.some((item)=>item.val === '')) {
-        return 'Game ended in draw';
-      }
-    }
-    return null;
-  }
-  render() {
-    const gameState = this.calculateWinner(this.state.gameSquares);
-    let status = '';
-    
-    if(gameState) {
-      status = gameState;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-    
-    return <div className={this.props.hide}>
-        <nav className="navbar navbar-inverse">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <a className="navbar-brand">
-                {status}
-              </a>
-            </div> 
-          </div>
-        </nav>
-        
-        <div>
-          {this.state.gameSquares.map((item, index) => {
-            return (
-              <GridItem 
-                key={item.id} 
-                val={this.state.gameSquares[index].val} 
-                onClick={() => this.updateSquare(index)} />
-            );
-          })}
-        </div>
-      </div>;
-  }
+function GameGrid(props) {
+  return(
+    <div>
+      {props.gameSquares.map((item, index) => {
+        return (
+          <GridItem 
+            key={item.id} 
+            val={item.val} 
+            onClick={() => props.onClick(index)} />
+        );
+      })}
+    </div>
+  );
 }
 
 export default GameGrid;
